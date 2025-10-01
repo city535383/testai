b6b6a0a7a4d4ae261c3f5aeac2466f8c5664416e
city535383 <145325822+city535383@users.noreply.github.com>
2025-10-01 19:37:49 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,7 +12,7 @@ This example uses p5 preload function to create the classifier
 // Classifier Variable
 let classifier;
 // Model URL
-let imageModelURL = 'https://teachablemachine.withgoogle.com/models/hk59cAHml/';
+let imageModelURL = 'https://teachablemachine.withgoogle.com/models/dS-n4CRv6/';
 
 // Video
 let video;
