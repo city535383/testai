8a93e4e8f46a717d492eb6afa2a388f224eb2030
city535383 <145325822+city535383@users.noreply.github.com>
2025-10-01 18:38:51 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,7 +12,7 @@ This example uses p5 preload function to create the classifier
 // Classifier Variable
 let classifier;
 // Model URL
-let imageModelURL = 'https://city535383.github.io/testai/';
+let imageModelURL = 'https://teachablemachine.withgoogle.com/models/RgMNc4Hg89/';
 
 // Video
 let video;
